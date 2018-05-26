4cd83f30c6ee72555793cfafbbba8dbc0c4e3064
rick <rick@ricks-MacBook-Air.local>
2018-05-26 14:56:22 -0400

    anyproxy plugin

---
+++ proxy.js
@@ -0,0 +1,36 @@
+_ = require('underscore'),
+    c = require('chalk'),
+    pj = require('prettyjson'),
+    request = require('request'),
+    l = console.log,
+    config = require('./config'),
+    uri = 'https://localhost:' + config.port + '/api';
+
+
+module.exports = {
+    summary: 'nodejs anyproxy rule',
+    * beforeSendResponse(requestDetail, responseDetail) {
+        if (requestDetail.url.includes('/cometd') && requestDetail.url.includes('chess.com') && 'response' in responseDetail && 'body' in responseDetail.response) {
+            l(c.red.bgBlack("*"));
+            if (responseDetail.response.body.includes('[{"data":{"game":{"id"')) {
+                console.log('received response for chess.com', typeof responseDetail, 'sending to', uri);
+                var dat = JSON.parse(responseDetail.response.body.toString());
+                l(typeof dat);
+                dat = dat[0].data.game;
+                l(pj.render(dat));
+                var options = {
+                    uri: uri,
+                    method: 'POST',
+                    json: dat,
+                };
+                request(options, function(error, response, body) {
+
+                    l('post completed');
+                    l(body);
+
+                });
+
+            }
+        }
+    },
+};
