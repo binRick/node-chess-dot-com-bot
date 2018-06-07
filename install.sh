0818806649860ff620c0ad79ae3d667bdb69ddd2
Richard Blundell <rblundell@deloitte.com>
2018-06-07 12:36:49 -0400

    firewalld open ports

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+AP_PORT=4911
 AP=~/anyproxy
 ls ~/.anyproxy/certificates/rootCA.crt >/dev/null 2>&1 || $AP/bin/anyproxy-ca
+which firewall-cmd >/dev/null 2>&1 && sudo firewall-cmd --add-port=8002/tcp --permanent && sudo firewall-cmd --add-port=${AP_PORT}/tcp --permanent && sudo firewall-cmd --reload
