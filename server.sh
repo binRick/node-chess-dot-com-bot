3ac1ab8dd3a5a3d39d4ced46060c908333cde441
Richard Blundell <rblundell@deloitte.com>
2018-06-07 12:32:36 -0400

    tidy up start script

--- server.sh
+++ server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-NODE_TLS_REJECT_UNAUTHORIZED="0" ~/Desktop/anyproxy/bin/anyproxy --port 4911 --intercept --ws-intercept --rule ~/Desktop/node-chess-dot-com-bot/proxy.js --silent
+AP=~/anyproxy
+AP_PORT=4911
+NODE_TLS_REJECT_UNAUTHORIZED="0" $AP/bin/anyproxy --port $AP_PORT --intercept --ws-intercept --rule ~/node-chess-dot-com-bot/proxy.js --silent
