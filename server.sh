03c1add016a4d5d88c4ecbd647bc1a35f141023d
Richard Blundell <rblundell@deloitte.com>
2018-06-07 12:34:26 -0400

    add silent mode selection

--- server.sh
+++ server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 AP=~/anyproxy
 AP_PORT=4911
-NODE_TLS_REJECT_UNAUTHORIZED="0" $AP/bin/anyproxy --port $AP_PORT --intercept --ws-intercept --rule ~/node-chess-dot-com-bot/proxy.js --silent
+AP_SILENT=0
+if [ "$AP_SILENT" == "1" ]; then export AP_SILENT="--silent"; else export AP_SILENT=""; fi
+
+NODE_TLS_REJECT_UNAUTHORIZED="0" $AP/bin/anyproxy --port $AP_PORT --intercept --ws-intercept --rule ~/node-chess-dot-com-bot/proxy.js $AP_SILENT
