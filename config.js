b57716f41fca5f07f3dfe9a731312c208da16f2b
rick <rick@ricks-MacBook-Air.local>
2018-06-07 21:06:12 -0400

    add chess engine tunables to config

--- config.js
+++ config.js
@@ -2,13 +2,24 @@ var _ = require('underscore'),
     os = require('os');
 
 module.exports = {
-    Threads: os.cpus.length-1,
-    Hash: 32,
-    depth: 15,
-    principalVariation: 3,
-    playerName: 'username',
-    mouseEventDelay: 35,
-    mouseMoveDelay: 30,
+	MultiPV 1, //Min: 1, Max: 500
+//The number of alternate lines of analysis to display. Specify 1 to just get the best line. Asking for more lines slows down the search.
+    Threads: os.cpus.length-1, //Min: 1, Max: 128
+//The number of threads to use during the search. This number should be set to the number of cores in your CPU.
+	
+    Hash: 16, //Min: 1, Max: 1048576
+	//The amount of memory to use for the hash during search, specified in MB (megabytes). This number should be smaller than the amount of physical memory for your system.
+	Skill: 20, //Min: 0, Max: 20
+//How well you want Stockfish to play. At level 0, Stockfish will make dumb moves. Level 20 is best/strongest play.
+	//
+//Min: 0, Max: 5000
+//The minimum amount of time to analyze, in milliseconds.
+	Ponder: true, //Whether or not the engine should analyze when it is the opponent's turn.
+    depth: 8,
+    principalVariation: 2,
+    playerName: 'chessbenzell',
+    mouseEventDelay: 1,
+    mouseMoveDelay: 1,
     port: 4912,
     engine: __dirname + '/stockfish-8-mac/Mac/stockfish-8-64',
     letters: 'a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9 ! ?'.split(' '),
